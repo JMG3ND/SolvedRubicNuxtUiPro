3eeb9f0577691433239fe8b151a048c4b24981cc
Jose Gutierrez <jo0303se@hotmail.com>
2024-07-05 11:39:55 -0500

    commit

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <UHeader>
+      <template #logo>
+        <NuxtImg class="w-36" src="logo-solvedrubik.png" alt="Logo" />
+      </template>
+      <template #right>
+        <UColorModeButton />
+      </template>
+    </UHeader>
+    <UMain>
+      <NuxtPage />
+    </UMain>
   </div>
 </template>
